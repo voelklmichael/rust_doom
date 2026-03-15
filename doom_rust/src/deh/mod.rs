9795439af26f55ebb8663d73d23ddbbd4388a027
Michael Voelkl <mv@zrch.com>
2026-03-15 21:08:53 +0100

    starting point

--- doom_rust/src/deh/mod.rs
+++
@@ -1,9 +0,0 @@
-//! DeHackEd support (stubs when FEATURE_DEHACKED is false).
-//!
-//! Original: deh_main.h, deh_misc.h, deh_str.h
-
-pub mod deh_main;
-pub mod deh_str;
-pub mod misc;
-
-pub use deh_str::deh_string;
--- doom_rust/src/info/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Thing/mobj info tables (mobjinfo_t, state_t).
-//!
-//! Original: info.h + info.c (minimal subset)
-
-pub mod tables;
-pub mod types;
-
-pub use tables::{states, MOBJINFO, MT_PLAYER, MT_TELEPORTMAN, NUMMOBJTYPES, NUMSTATES};
-pub use types::{Mobjinfo, Mobjtype, Spritenum, State, Statenum, S_NULL};
